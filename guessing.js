94b1a64e05fd9946bd4422dd5a292cbd2bea810d
sujji1729 <sujathanu96@gmail.com>
2024-08-01 17:37:45 +0530

    Guessing Game

---
+++ guessing.js
@@ -0,0 +1,35 @@
+let userInput =  document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+let gameResult = document.getElementById("gameResult");
+
+function checkGuess() {
+    
+    let guessNumber = parseInt(userInput.value);
+
+    if (guessNumber > randomNumber) {
+
+        gameResult.textContent = "Too High! Try Again."; 
+        gameResult.style.backgroundColor = "#1e217c";
+
+    }
+
+    else if (guessNumber < randomNumber) {
+
+        gameResult.textContent = "Too Low! Try Again."; 
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+
+    else if (guessNumber === randomNumber) {
+
+        gameResult.textContent = "Congratulations! You got it right.";
+        gameResult.style.backgroundColor = "green";
+    }
+
+    else{
+
+        gameResult.textContent = "Please provide a valid input.";
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+
+
+}
